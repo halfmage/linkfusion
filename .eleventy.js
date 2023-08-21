ac36058e44af541755b5e20bb5a3a915db38cffa
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-08-21 11:37:53 +0200

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/add-link.js");
     eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
         name: "linkfusion", // The serverless function name from your permalink object
         functionsDir: "./netlify/functions/",
     });
+    
     return {
         dir: {
             input: "src",
